9cdb4948058fa3ebfdaf0f182d151084fd0c815b
XT <1406013094@qq.com>
2024-05-13 18:49:31 +0800

    新增feign、日志打印切面

---
+++ test1/src/main/java/com/test1/config/LogAspectController.java
@@ -0,0 +1,68 @@
+package com.test1.config;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * @author XT
+ * @Title: LogAspectController
+ * @Date: 2024/5/11 17:08
+ * @description: 日志切面配置类
+ */
+@Aspect
+@Slf4j
+@Component
+public class LogAspectController {
+
+
+    /**
+     * 申明切点
+     */
+    @Pointcut("execution(public * com.test1.controller..*.*(..))")
+    private void controllerAspect() {
+
+    }
+
+    /**
+     * 执行方法前打印
+     * @param joinPoint
+     */
+    @Before(value = "controllerAspect()")
+    public void methodBefore(JoinPoint joinPoint){
+        Map<String,String[]> map;
+        ServletRequestAttributes servletRequestAttributes =  (ServletRequestAttributes) RequestContextHolder
+               .getRequestAttributes();
+        if(servletRequestAttributes != null){
+            HttpServletRequest request = servletRequestAttributes.getRequest();
+            map = request.getParameterMap();
+            if(map != null && !map.isEmpty()){
+                log.info("method: " + joinPoint
+                        + ",url: " + request.getRequestURI()
+                        + ",requestMethod: " + request.getMethod()
+                        + ",parameter: " + JSONObject.toJSONString(request.getParameterMap()));
+            }else {
+                Object[] args = joinPoint.getArgs();
+                if (null != args && args.length > 0){
+                    log.info("method: " + joinPoint
+                            + ",url: " + request.getRequestURI()
+                            + ",requestMethod: " + request.getMethod()
+                            + ",parameter: " + StringUtils.join(args));
+                }
+            }
+        }
+
+    }
+
+
+}
--- test2/src/main/java/com/test2/Test2Application.java
+++ test2/src/main/java/com/test2/Test2Application.java
@@ -3,6 +3,7 @@ package com.test2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author XT
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class Test2Application {
     public static void main(String[] args) {
         SpringApplication.run(Test2Application.class,args);
